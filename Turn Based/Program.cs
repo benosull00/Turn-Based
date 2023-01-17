057fb4b6114ac032f74ee81c3e6330d163068f47
benosull00 <ben.osull00@gmail.com>
2023-01-17 23:19:04 +0000

    basic class shizz

--- Turn Based/Program.cs
+++ Turn Based/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace Turn_Based
+Console.WriteLine("Select a class");
+Console.WriteLine("\n1. Tank\n2. Fighter\n3. Mage");
+string classSelectText = Console.ReadLine();
+int classSelect = Convert.ToInt32(classSelectText);
+
+switch (classSelect)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    case 1:
+        Console.WriteLine("You chose Tank\n\nHP: 200\n\nWeapons:\nGreatsword\nShield");
+        break;
+
+    case 2:
+        Console.WriteLine("You chose Fighter\n\nHP: 140\n\nWeapons:\nShortsword x2");
+        break;
+
+    case 3:
+        Console.WriteLine("You chose mage\n\nHP: 100\n\nWeapons:\nStaff of Fire\nStaff of Ice");
+        break;
 }
+
+Console.WriteLine("gIT TEST");
