7060730a805f2633be369eebf386d8bcd4c8328d
benosull00 <ben.osull00@gmail.com>
2023-01-25 13:22:43 +0000

    making classes

---
+++ Turn Based/Mage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turn_Based
+{
+    internal class Mage : Character
+    {
+        public Mage()
+        {
+            this.name = Console.ReadLine();
+            this.health = 100;
+            this.damage = 40;
+            this.block = 6;
+        }
+    }
+}
